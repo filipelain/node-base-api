dc9ecf83f276bd178c765664c571a8a9e6424030
Filipe Rodrigues Barretto <filipe.barretto@senaicni.com.br>
2020-05-04 10:37:46 -0300

    Teste com Autenticação

--- express-admin/spec/serves.spec.js
+++ express-admin/spec/serves.spec.js
@@ -7,36 +7,50 @@ describe("server functionality", () => {
     let server;
     beforeAll(() => server = require("../app"));
 
-    describe("GET /",()=>{
+    let token = 1;
+    let option = {
+        url: baseUrl,
+        Authorization: `Bearer ${token}`
+
+    }
+
+    describe("GET /", () => {
         let data = {};
-        beforeAll((done)=>{
-            Request.get(baseUrl,(error, response,body)=>{
+        //headers: { Authorization: `Bearer ${token}` }
+        beforeAll((done) => {
+            Request.get(baseUrl,
+                { headers: { 
+                    'Authorization': 'bearer 1' 
+                }}
+                , (error, response, body) => {
+                    if (error) {
+                        console.log('error', error)
+                    }
                     data.status = response.statusCode;
                     data.body = JSON.parse(body);
                     done();
-            })
+                })
         });
-        it("status 200",()=>{
+        it("status 200", () => {
             expect(data.status).toBe(200);
         });
-        it("body is Hello World!",()=>{
-            expect(data.body.service).toBe('ok');
-        });
+         it("body is Hello World!", () => {
+             expect(data.body.service).toBe('ok');
+         });
     });
 
-    describe("GET /users",()=>{
-        data = {};
-        beforeAll((done)=>{
-            Request.get(baseUrl+"users",(error, response,body)=>{
-                    data.status = response.statusCode;
-                    done();
-            });
-        });
-
-        it("users not authorized",()=>{
-            expect(data.status).toBe(401);
-        })
-    });
+     describe("GET /users", () => {
+         data = {};
+         beforeAll((done) => {
+             Request.get(baseUrl + "users", (error, response, body) => {
+                 data.status = response.statusCode;
+                 done();
+             });
+         })
+         it("users not authorized", () => {
+             expect(data.status).toBe(401);
+         })
+     });
 
     afterAll(() => server.close());
 });
